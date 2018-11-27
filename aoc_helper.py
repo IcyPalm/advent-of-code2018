d9e1b91897e31de945d0ad04be0419594a7fd837
Marijn Pool <info@marijnpool.nl>
2018-11-27 22:50:09 +0100

    Session token is now checked

--- aoc_helper.py
+++ aoc_helper.py
@@ -70,13 +70,35 @@ def create_day_setup():
 
 
 def check_session_token(new_session_token=''):
+    # If a new session token is set, take that one
     if new_session_token:
-        set_aoc_session(new_session_token)
-    if not AOC_SESSION_TOKEN:
+        logging.debug('New session token given, setting that')
+        session_token = new_session_token
+    # If not a new token is given and there is no environment variable set with the token, ask for one
+    elif not AOC_SESSION_TOKEN:
+        logging.debug('No new and no environment varible with Token, asking user for new token')
         click.echo('It seems that you have not set your AOC session token!')
         session_token = click.prompt('Please enter your AOC session token', type=str)
-        # TODO: Checking if session token is valid
-        set_aoc_session(session_token)
+    else:
+        logging.debug('We have a token, setting that')
+        session_token = AOC_SESSION_TOKEN
+    url = 'https://adventofcode.com/'
+    token_valid = False
+    while not token_valid:
+        logging.debug('testing if session token is valid')
+        r = requests.get(url=url, cookies=dict(session=session_token))
+        user_div = BeautifulSoup(r.content, 'html.parser').find('div', class_='user')
+        if not user_div:
+            logging.debug('Token is invalid, asking user for a new one')
+            click.echo('It seems that the session token is invalid')
+            session_token = click.prompt('Please enter your AOC session token', type=str)
+        else:
+            logging.debug('User div is found, session is valid')
+            username = str(user_div.string)
+            click.echo('It seems that the token is valid')
+            click.echo(f'Hello {username}')
+            token_valid = True
+    set_aoc_session(session_token)
 
 
 def set_aoc_session(new_session_token):
@@ -108,7 +130,7 @@ def get_day(day_input):
     else:
         day = guess_day()
         if day > 0:
-            if click.confirm(f'Looks like the day you want is: {day}, correct?'):
+            if click.confirm(f'Looks like the day you want to get is: {day}, correct?'):
                 return day
     return click.prompt('Please fill in the day(1-31)', type=click.IntRange(1, 31))
 
