f6d6a2a20ac11696135595b0b3e5fb5b2a2a0965
Marijn Pool <info@marijnpool.nl>
2018-12-02 12:29:54 +0100

    Day 2 Part 2 finished

--- day_02/day_02.py
+++ day_02/day_02.py
@@ -1,8 +1,9 @@
 import os
 from collections import Counter
+from itertools import combinations
 
 with open(os.path.join(os.path.dirname(__file__), 'input')) as input_file:
-    data = input_file.readlines()
+    data = input_file.read().splitlines()
 
 
 def part1():
@@ -18,8 +19,10 @@ def part1():
 
 
 def part2():
-    answer = "Part two answer"
-    return answer
+    for first, second in combinations(data, 2):
+        compare = [i for i, j in zip(first, second) if i == j]
+        if len(first) - len(compare) == 1:
+            return "".join(compare)
 
 
 if __name__ == "__main__":
